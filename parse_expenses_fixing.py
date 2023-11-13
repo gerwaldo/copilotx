fbf161fffe55e4206ec93517c4aabf5dc428d60d
Gerwald Oberleitner <gerwaldo@users.noreply.github.com>
2023-11-13 13:48:39 +0000

    code fixes

--- parse_expenses_fixing.py
+++ parse_expenses_fixing.py
@@ -14,18 +14,12 @@ def parse_expenses (expenses_string) :
     for line in expenses_string.splitlines():
         if line.startswith("#"):
             continue
-        date, value, currency = line.split(",")
-        expenses.append((float (value),
-                        currency,
-                        datetime.datetime.strptime(date, "%Y-%m-%d")))
+        date, value, currency = line.split()  # Changed from "," to " "
+        expenses.append((datetime.datetime.strptime(date, "%Y-%m-%d"),
+                        float(value),
+                        currency))
     return expenses
 
 expenses_data = '''2023-01-02 -34.01 USD
 2023-01-03 2.59 DKK
-2023-01-03 -2.72 EUR'''
-
-expenses = parse_expenses (expenses_data)
-for expense in expenses:
-    print (f'{expense[0]} {expense[1]} {expense[2]}')
-
-
+'''
